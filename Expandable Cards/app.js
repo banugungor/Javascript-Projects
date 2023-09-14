fb13ee08230e6120eaf6e7caf04f453d28502322
Banu <banuuzun9@gmail.com>
2023-09-14 18:11:25 +0300

    js

--- Expandable Cards/app.js
+++ Expandable Cards/app.js
@@ -2,6 +2,12 @@ const panels = document.querySelectorAll(".panel");
 
 panels.forEach((panel) => {
   panel.addEventListener("click", () => {
+    removeActive();
     panel.classList.add("active");
   });
 });
+function removeActive(){
+  panels.forEach((panel)=>{
+    panel.classList.remove("active")
+  })
+}
